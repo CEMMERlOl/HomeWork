89e4eacf166319e1fe6b6f343b523fd5e722bad9
CEMMERlOl <107998084+CEMMERlOl@users.noreply.github.com>
2022-07-07 01:37:09 +0300

    Заходит на ютуб через страницу поиска яндекса
    Если всплывающие окна запрещены, работать не будет

---
+++ yandexBot.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         yandexBot
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  For yandex
+// @author       User
+// @match        https://yandex.ru/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+
+let keyword = document.getElementsByClassName("input__input")[0]
+if (keyword !== undefined){
+keyword.value = "youtube";
+document.getElementsByClassName("mini-suggest_separate-popup_yes")[0].submit();
+} else {
+setTimeout( function(){document.querySelector('a[href*="youtube.com"]').click();}, 100);
+}
